50f74b6b09fbb8379b6789c8d8694a1aedc9c489
Julian Metzler <git@mezgr.de>
2020-08-29 12:05:52 +0200

    RasPi: Add door states and LCD contrast to env log

--- RasPi/log_env.py
+++ RasPi/log_env.py
@@ -14,8 +14,10 @@ bl_fan = fia.get_backlight_ballast_fans_state()
 circ_fan = fia.get_circulation_fans_state()
 exch_fan = fia.get_heat_exchanger_fan_state()
 heaters = fia.get_heaters_state()
+doors = fia.get_door_states()
+contrast_a, contrast_b = fia.get_lcd_contrast()
 
-values = ",".join(map(str, (env_brt_a, env_brt_b, bl_brt_a, bl_brt_b, bl_base_brt_a, bl_base_brt_b, bl_state, bl_temp, air_temp, board_temp, mcu_temp, hum, bl_fan, circ_fan, exch_fan, heaters)))
+values = ",".join(map(str, (env_brt_a, env_brt_b, bl_brt_a, bl_brt_b, bl_base_brt_a, bl_base_brt_b, bl_state, bl_temp, air_temp, board_temp, mcu_temp, hum, bl_fan, circ_fan, exch_fan, heaters, doors, contrast_a, contrast_b)))
 
 with open("env_log.csv", 'a') as f:
     f.write(f"{time.time():.0f},{values}\n")
